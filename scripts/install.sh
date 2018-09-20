8f88b5476cf02a5e31349b010099175432f534ce
bik <dzeckelev@gmail.com>
2018-09-20 13:09:14 +0700

    Change config name to `dappvpn.config.json` Change default  name of a adapter`s executable file Add flags description Change install description

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,17 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 GIT_RELEASE=$(git tag -l --points-at HEAD)
 ADAPTER_MAIN=/adapter
 INSTALLER_MAIN=/installer
+ADAPTER_NAME=dappvpn
+INSTALLER_NAME=installer
 
 cd "${PROJECT_PATH}" || exit
 dep ensure
 go generate ./...
 
-go install -ldflags "-X main.Commit=$GIT_COMMIT \
+go build -o $GOPATH/bin/${ADAPTER_NAME} -ldflags "-X main.Commit=$GIT_COMMIT \
     -X main.Version=$GIT_RELEASE" -tags=notest \
     ${PROJECT}${ADAPTER_MAIN}
 
-go install -ldflags "-X main.Commit=$GIT_COMMIT \
+go build -o $GOPATH/bin/${INSTALLER_NAME}  -ldflags "-X main.Commit=$GIT_COMMIT \
     -X main.Version=$GIT_RELEASE" -tags=notest \
     ${PROJECT}${INSTALLER_MAIN}
