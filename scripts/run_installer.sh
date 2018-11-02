a215e57c32f039a653d0a2682815795eafd1fb43
drew2a <for.drew@gmail.com>
2018-11-02 15:51:00 +0300

    add cp to bin

--- scripts/run_installer.sh
+++ scripts/run_installer.sh
@@ -2,11 +2,16 @@
 
 if [ -z "${POSTGRES_PORT}" ]
 then
-    POSTGRES_PORT=5433
+    POSTGRES_PORT=5432
 fi
 
+EXAMPLE_DIR="$GOPATH/src/github.com/privatix/dapp-openvpn/files/example"
+BIN_DIR="$GOPATH/src/github.com/privatix/dapp-openvpn/bin"
+
+cp -a ${EXAMPLE_DIR}/. ${BIN_DIR}/example
+
 # Example run installer
-echo dapp-openvpn-inst \
- -rootdir="$GOPATH/src/github.com/privatix/dapp-openvpn/files/example" \
+dapp-openvpn-inst \
+ -rootdir=${BIN_DIR}/example \
  -connstr="dbname=dappctrl host=localhost user=postgres \
   sslmode=disable port=${POSTGRES_PORT}" -setauth
