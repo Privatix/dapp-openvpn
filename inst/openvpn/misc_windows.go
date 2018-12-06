484598baac46ea6e84622a5c3967f9769bdb9ce4
Urmat Bozov <urmat@privatix.io>
2018-12-06 22:28:26 +0600

    BV-1177

--- inst/openvpn/misc_windows.go
+++ inst/openvpn/misc_windows.go
@@ -5,3 +5,11 @@ import "fmt"
 func serviceName(prefix, path string) string {
 	return fmt.Sprintf("%s_%s", prefix, hash(path))
 }
+
+func networkInterface() (string, error) {
+	return "", nil
+}
+
+func openvpnInterface() string {
+	return ""
+}
