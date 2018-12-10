1a4055d928a71191b2638ed487195806d44f50f1
Andrei Tseitlin <andrei@privatix.io>
2018-12-10 18:43:06 +0200

    Schedule task to fix ICS windows bug.

---
+++ scripts/win/new-startupTask.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    Creates scheduled task to re-enable Internet Connection Sharing and restart OpenVPN server.
+.DESCRIPTION
+    There is a known bug with ICS (Internet Connection Sharing), where it must be re-enabled after restart. 
+    This script will create scheduled task for this operation.
+
+.PARAMETER scriptPath
+    Path to reenable-nat.ps1 script.
+
+.PARAMETER TAPdeviceAddress
+    Unique identifier of TAP device. It is identified by "PnPDeviceID" (Get-NetAdapter) and same as "Device instance path" in device manager.
+
+.EXAMPLE
+    In cmd.exe with admin priveledges run following command: 
+    powershell.exe -executionpolicy bypass -noprofile -file `
+    "C:\Program Files\Privatix\agent\product\73e17130-2a1d-4f7d-97a8-93a9aaa6f10d\bin\new-startupTask.ps1" `
+    -scriptPath "C:\Program Files\Privatix\agent\product\73e17130-2a1d-4f7d-97a8-93a9aaa6f10d\bin\reenable-nat.ps1" `
+    -TAPdeviceAddress "ROOT\NET\0000"
+
+    Description
+    -----------
+    Creates scheduled task for ICS re-enabling.
+
+#>
+Param(
+    [ValidateScript( {Test-Path $_})]
+    [string]$scriptPath,
+    [string]$TAPdeviceAddress
+)
+#Requires -RunAsAdministrator
+
+$RandomDelay = New-TimeSpan -Minute 1
+$TaskTrigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay $RandomDelay
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-executionpolicy bypass -noprofile -file `"$scriptPath`" -TAPdeviceAddress `"$TAPdeviceAddress`""
+$User = "NT AUTHORITY\System"
+Register-ScheduledTask -TaskName "Privatix re-enable ICS" -Trigger $TaskTrigger -User $User -Action $Action -RunLevel Highest -Force -Description "Internet connection sharing must be re-enabled after computer restart, due to Microsoft bug."
